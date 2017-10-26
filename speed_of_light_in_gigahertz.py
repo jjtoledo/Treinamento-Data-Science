79a8d33b118780de201bf8adbea830894e2b7614
José Cassimiro <jjtoledo93@gmail.com>
2017-10-26 17:31:34 -0200

    exercícios iniciais

---
+++ speed_of_light_in_gigahertz.py
@@ -0,0 +1,7 @@
+# speed_of_light in meters per second
+# cycles_per_second is 2.7 GHz
+
+speed_of_light    = 299792458.0
+cycles_per_second = 2700000000.0
+
+print speed_of_light / cycles_per_second
---
+++ variables.py
@@ -0,0 +1,6 @@
+# What is the value of hours after running this code:
+hours = 9
+hours = hours + 1
+hours = hours * 2
+
+print hours
