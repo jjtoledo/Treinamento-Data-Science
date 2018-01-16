1b85bc65c7be1a069b1ebcb192afff88bb61176a
José Cassimiro <jjtoledo93@gmail.com>
2018-01-16 14:19:09 -0200

    random words

---
+++ random_word.py
@@ -0,0 +1,41 @@
+'''We used `import random` and then the function definition was simply:
+```python
+def generate_password():
+	return random.choice(word_list) + random.choice(word_list) + random.choice(word_list)
+```
+Alternatively, you could use the `random.sample` function and `.join` method for strings:
+```
+def generate_password():
+    return str().join(random.sample(word_list,3))
+```
+'''
+
+# Use an import statement at the top
+from random import randint
+
+word_file = "words.txt"
+word_list = []
+
+# fill up the word_list
+with open(word_file, 'r') as words:
+    for line in words:
+        # remove white space and make everything lowercase
+        word = line.strip().lower()
+        # don't include words that are too long or too short
+        if 3 < len(word) < 8:
+            word_list.append(word)
+
+# Add your function generate_password here
+# It should return a string consisting of three random words
+# concatenated together without spaces
+
+
+def generate_password():
+    string = ''
+    for i in [0, 1, 2]:
+        index = randint(0, len(word_list) - 1)
+        string += word_list[index]
+    return string
+
+
+print(generate_password())
